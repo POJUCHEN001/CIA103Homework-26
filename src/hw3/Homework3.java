892ee419b8fd91ba3a76c3b27bbdd74b2453a177
P.J.Chen <lulu.chen231@gmail.com>
2024-08-24 10:13:41 +0800

    add hw3

---
+++ src/hw3/Homework3.java
@@ -0,0 +1,31 @@
+package hw3;
+
+import java.util.Scanner;
+
+public class Homework3 {
+	public static void main(String[] args) {
+		//1.請設計一隻程式，使用者輸入三個數字後，輸出結果會為正三角形、等腰三角形、其它三角形或不是三角形
+		Scanner sc =new Scanner(System.in);
+		System.out.println("請輸入三個整數：");
+//		if(sc.hasNextInt()) {
+//			i = sc.nextInt();
+//		}
+//		if(sc.hasNextDouble()) {
+//			d = sc.nextDouble();
+//		}
+		
+		//提示：Scanner，三角形成立條件，判斷式if else
+		//進階功能：加入直角三角形的判斷
+		
+		
+		//2.請設計一隻程式，會亂數產生一個0～9的數字，然後可以玩猜數字遊戲，猜錯會顯示錯誤訊息，猜對則顯示正確訊息
+		
+		//提示：Scanner，亂數方法，無窮迴圈
+		//進階功能：產生 0 ~ 100 亂數，每次猜就會提示你是大於還是小於正確答案
+		
+		//3.阿文很喜歡簽大樂透(1～49)，請設計一隻程式，讓阿文可以輸入他不想要的數字(1～9)，畫面會顯示他可以選擇的號碼與總數
+		
+		//提示：Scanner
+		//進階挑戰：輸入不要的數字後，直接亂數印出6個號碼且不得重複
+	}
+}
